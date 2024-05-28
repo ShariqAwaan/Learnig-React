f676bfadc4bc05ba48542292c99e9353171ec62c
Muhammad Shariq Abdullah <145561688+ShariqAwaan@users.noreply.github.com>
2024-05-28 16:23:07 +0500

    Making a new react app for revising the JS Assignments in React

---
+++ jsassignmentsrecall/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import JSAssignments from './components/JSAssignments';
+
+function App() {
+  return (
+    <>
+    <JSAssignments />
+    </>
+  );
+}
+
+export default App;
---
+++ jsassignmentsrecall/src/components/JSAssignments.js
@@ -0,0 +1,37 @@
+import React from 'react'
+
+function JSAssignments() {
+  return (
+    <>
+    <div id="mainContainer">
+      <h3 style={{color: 'green'}}>JS Assignments Recall</h3>
+      <div className="exercise1">
+      <h5>Q: Function that returns the square of a number</h5>
+      <label htmlFor="questionNo1">Place your value here : </label>
+      <input type="text"  /> <br />
+      <p></p>
+      </div>
+      <div className="exercise1">
+      <h5>Q: Function that returns the square of a number</h5>
+      <label htmlFor="questionNo1">Place your value here : </label>
+      <input type="text"  /> <br /> 
+      <p></p>
+      </div>
+      <div className="exercise1">
+      <h5>Q: Function that returns the square of a number</h5>
+      <label htmlFor="questionNo1">Place your value here : </label>
+      <input type="text"  /> <br />
+       <p></p> 
+      </div>
+      <div className="exercise1">
+      <h5>Q: Function that returns the square of a number</h5>
+      <label htmlFor="questionNo1">Place your value here : </label>
+      <input type="text"  /> <br /> 
+      <p></p>     
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default JSAssignments
