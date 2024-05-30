3fb740670585d964445e2af687439ff52fbd501a
Muhammad Shariq Abdullah <145561688+ShariqAwaan@users.noreply.github.com>
2024-05-30 22:04:38 +0500

    ConditionRendering Using If else conditions

--- ConditionRendering/src/Components/CRMethodfour.jsx
+++ ConditionRendering/src/Components/CRMethodfour.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
 
+function Time(hour){
+  if(hour < 0 || hour > 24 ){
+    return ("Invalid Time")
+  } else if(hour < 12){
+    return ("Good Morning")
+  } else {
+    return ("Good Noon")
+  }
+  
+}
+
 function CRMethodfour() {
+  // function Time(hour){
+  // if(hour < 0 || hour > 24 ){
+  //   return ("Invalid Time")
+  // } else if(hour < 12){
+  //  return ("Good Morning")
+  // } else {
+  //   return ("Good Noon")
+  // }
+// }
   return (
-    <div>CRMethodfour</div>
+    <div>
+    {Time(66)}
+    {/* <Time hour={33}/> */}
+    </div>
   )
 }
 
