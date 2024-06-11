12c95a571fd513cc92f597d3da4df020f3e9f29c
Muhammad Shariq Abdullah <145561688+ShariqAwaan@users.noreply.github.com>
2024-06-11 17:02:57 +0500

    practiced a sik=mple problem of useEffect to understand what is useEffect

--- firstapp/src/App.js
+++ firstapp/src/App.js
@@ -1,117 +1,83 @@
 import './App.css';
 import Objects from './components/4.Objects';
 import Immer from './components/5.UsingImmer';
-// import Default from './components/3.Passingprops/Default.jsx';
-// import Default from './components/3.Passingprops/Default';
-// import Conditionals from './components/Conditionals';
-// import Myfirstcomponent from './components/myfirstcomponent';
+import Default from './components/3.Passingprops/Default.jsx';
+import Conditionals from './components/Conditionals';
+import Myfirstcomponent from './components/myfirstcomponent';
 
-// import Profile from './components/Profile';
-// import Profile from "./components/3.Passingprops/Profile.jsx";
-// import AdjustingTheSize from './components/3.Passingprops/Adjust.the.image.size.based.on.a.prop';
-// import PassingChild from './components/3.Passingprops/Card.component.and.passing.child';
-// import Card from './components/3.Passingprops/Card.component';
-
-// export default function App(){
-//   return (
-    
-//   <>
-//   <Default name = "Shariq" gender = "Male" />
-//   </>
-//   );
-// }
+import Profile from "./components/3.Passingprops/Profile.jsx";
+import AdjustingTheSize from './components/3.Passingprops/Adjust.the.image.size.based.on.a.prop';
+import PassingChild from './components/3.Passingprops/Card.component.and.passing.child';
+import TestingThings from './components/6.TestingThings.jsx';
+import Card from './components/3.Passingprops/Card.component';
 
 
-// export default function App(){
-
-//   let obj1 = {
-//         name: "Maria Skłodowska-Curie" ,
-//         profession: "physicist and chemist" ,
-//         awards: "(Nobel Prize in Physics, Nobel Prize in Chemistry, Davy Medal, Matteucci Medal)" ,
-//         discovery: "polonium (chemical element)"
-//       }
-//   let obj2 = {
-//         name: "Katsuko Saruhashi" ,
-//         profession: "geochemist" ,
-//         awards: "(Miyake Prize for geochemistry, Tanaka Prize)" ,
-//         discovery: "a method for measuring carbon dioxide in seawater"
-//       }
-
-//   return (
-    
-//   <>
-//   <Default maria={obj1} katsuko = {obj2} />
-//   {/* <Profile /> */}
-//   </>
-//   );
-// }
 
 
-// export default function App(){
-//   return (
-    
-//   <>
-//   <PassingChild />
-//   </>
-//   );
-// }
 export default function App(){
-  return (
-    
-  <>
-  <h5>Simple Methods</h5>
-  <Objects /> 
+  
+        let obj1 = {
+        name: "Maria Skłodowska-Curie" ,
+        profession: "physicist and chemist" ,
+        awards: "(Nobel Prize in Physics, Nobel Prize in Chemistry, Davy Medal, Matteucci Medal)" ,
+        discovery: "polonium (chemical element)"
+        }
+        let obj2 = {
+          name: "Katsuko Saruhashi" ,
+          profession: "geochemist" ,
+          awards: "(Miyake Prize for geochemistry, Tanaka Prize)" ,
+          discovery: "a method for measuring carbon dioxide in seawater"
+          }
+          
+          return (
+            
+            <>
 
-  <h5>Using Immer</h5>
-  <Immer />
-  </>
+            <h5>Default</h5>
+            <Card>
+            <Default maria={obj1} katsuko = {obj2} />
+            </Card>
+            
+            {/* <h5>Profile</h5>
+            <Profile />
+            <Profile maria={obj1} katsuko = {obj2} />
+            <Profile name = "D" clas = "Web D"/> */}
+               
+            <h5>Passing Childs</h5>
+            <PassingChild />
+
+
+            <h5>Adjusting Size</h5>
+            <Card>
+            <AdjustingTheSize />
+            </Card>
+
+
+            <h5>Conditionals</h5>
+            <Card>
+            <Conditionals />
+            </Card>
+            <h5>My First Component</h5>
+            <Card>
+            <Myfirstcomponent />
+            </Card>
+
+
+            <h5>Simple Methods</h5>
+            <Card>
+            <Objects /> 
+            </Card>
+
+            <h5>Using Immer</h5>
+            <Card>
+            <Immer />
+            </Card>
+
+            <h5>Testing Things</h5>
+            <Card>
+            <TestingThings />
+            </Card>
+            </>
   );
 }
 
-
-// export default function App(){
-//   return (
-    
-//   <>
-//   <AdjustingTheSize />
-//   </>
-//   );
-// }
-
-
-// export default function App() {
-//   let obj1 = {
-//     name: "Maria Skłodowska-Curie" ,
-//     profession: "physicist and chemist" ,
-//     awards: "(Nobel Prize in Physics, Nobel Prize in Chemistry, Davy Medal, Matteucci Medal)" ,
-//     discovery: "polonium (chemical element)"
-//   }
-//   let obj2 = {
-//     name: "Katsuko Saruhashi" ,
-//     profession: "geochemist" ,
-//     awards: "(Miyake Prize for geochemistry, Tanaka Prize)" ,
-//     discovery: "a method for measuring carbon dioxide in seawater"
-//   }
-  
-  
-
-//   return (
-//     <>
-//       <Profile maria={obj1} katsuko = {obj2} />
-//     </>
-//   );
-// }
-
-
-// function App() {
-//   return (
-//     <>
-//       <Profile name = "D" clas = "Web D"/>
-//       <Conditionals />
-//       <Myfirstcomponent />
-
-//     </>
-//   );
-// }
-
-// export default App;
---
+++ firstapp/src/components/6.TestingThings.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { useState, useEffect } from 'react';
+
+function TestingThings() {
+
+
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    document.title = `You clicked ${count} times`;
+  }, [count]);
+
+  return (
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>
+        Click me
+      </button>
+    </div>
+  );
+
+
+}
+
+export default TestingThings
